e55fba4fc0e36234ab79b91004730dc7bef4bb1f
Alex <alexandermora@live.no>
2025-03-28 15:23:57 +0100

    Create new hook for Expo Push Notifications

---
+++ frontend/greenhouse-controller/src/hooks/usePushNotifications.tsx
@@ -0,0 +1,89 @@
+import { useState, useEffect, useRef } from "react";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants";
+import { Platform } from "react-native";
+
+// Code based on this tutorial: https://www.youtube.com/watch?v=V-hois8dgM4
+
+export interface PushNotificationState {
+  expoPushToken?: Notifications.ExpoPushToken;
+  notification?: Notifications.Notification;
+}
+
+export const usePushNotifications = (): PushNotificationState => {
+  const [expoPushToken, setExpoPushToken] = useState<Notifications.ExpoPushToken | undefined>();
+  const [notification, setNotification] = useState<Notifications.Notification | undefined>();
+
+  const notificationsListener = useRef<Notifications.EventSubscription>();
+  const responseListener = useRef<Notifications.EventSubscription>();
+
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldPlaySound: false,
+      shouldShowAlert: true,
+      shouldSetBadge: false,
+    }),
+  });
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+
+    if (Device.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+
+      let finalStatus = existingStatus;
+
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token");
+      }
+
+      token = await Notifications.getExpoPushTokenAsync({
+        projectId: Constants.expoConfig?.extra?.eas?.projectId,
+      });
+    } else {
+      console.log("ERROR: Please use physical device");
+    }
+
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    return token;
+  }
+
+
+    useEffect(() => {
+      registerForPushNotificationsAsync().then((token) => {
+        setExpoPushToken(token);
+      });
+
+      notificationsListener.current = Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+      responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+      return () => {
+        Notifications.removeNotificationSubscription(notificationsListener.current!);
+
+        Notifications.removeNotificationSubscription(responseListener.current!);
+      };
+    }, [])
+
+    return {
+      expoPushToken,
+      notification,
+    };
+  };
